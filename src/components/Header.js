b1c343e2c0d7aee62ddd83cae209980a5a9ee32a
Robin Steinhauser <rbn2208@gmail.com>
2021-02-04 14:00:16 +0100

    fix header import

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import './Header.css'
 
-export default function Headers() {
+export default function Header() {
   const el = document.createElement('header')
   el.className = 'Header'
   el.innerHTML = `
-    <h1 class="header__title">
+    <h1 class="Header__title">
     Quiz App
-    <span class="header__subtitle">May the best win!</span>
-  </h1>
+    <span class="Header__subtitle">May the best win!</span>
+    </h1>
     `
   return el
 }
