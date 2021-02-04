3a05f7dc5c6ea91c1d78c501dc05db0d07b8a2ba
Robin Steinhauser <rbn2208@gmail.com>
2021-02-04 14:14:48 +0100

    next fix with heading import, now only js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import Button from './components/Button'
+import Header from './components/Header'
 
 console.log('hello world')
 
+const header = Header()
 const button = Button('Click me')
 const button1 = Button('Click this!')
 
+document.body.append(header)
 document.body.append(button)
 document.body.append(button1)
