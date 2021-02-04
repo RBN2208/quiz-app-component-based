255f94e5f58b4beb0b803f27028016994ed0e428
Robin Steinhauser <rbn2208@gmail.com>
2021-02-04 16:15:22 +0100

    adjust header js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,14 @@ import createElement from '../lib/createElement'
 import './Header.css'
 
 export default function Header(title, subtitle) {
-  const el = createElement('header', { className: 'Header' })
-  el.innerHTML = `
-    <h1 class="Header__title">
-    ${title}
-    <span class="Header__subtitle">${subtitle}</span>
-    </h1>
-    `
+  const el = createElement('header', {
+    className: 'Header',
+    innerHTML: `
+  <h1 class="Header__title">
+  ${title}
+  <span class="Header__subtitle">${subtitle}</span>
+  </h1>
+  `,
+  })
   return el
 }
