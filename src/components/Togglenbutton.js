0401cda6e21540c11e65a686219f0f6767b3fbd6
Robin Steinhauser <rbn2208@gmail.com>
2021-02-05 08:56:36 +0100

    added darkmodebutton with function

---
+++ src/components/Togglenbutton.js
@@ -0,0 +1,24 @@
+import './Togglebutton.css'
+import createElement from '../lib/createElement'
+
+export default function Togglebutton() {
+  const inputEl = createElement('input')
+  inputEl.setAttribute('type', 'checkbox')
+
+  const spanEl = createElement('span', { className: 'Togglebutton__slider' })
+
+  const labelEl = createElement(
+    'label',
+    { className: 'Togglebutton' },
+    inputEl,
+    spanEl
+  )
+
+  inputEl.addEventListener('change', () => {
+    const currentTheme = document.documentElement.getAttribute('data-theme')
+    const switchToTheme = currentTheme === 'dark' ? 'light' : 'dark'
+    document.documentElement.setAttribute('data-theme', switchToTheme)
+  })
+
+  return labelEl
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Button from './components/Button'
 import Header from './components/Header'
 import Card from './components/Card'
 import createElement from './lib/createElement'
+import Togglebutton from './components/Togglenbutton'
 
 const grid = createElement(
   'div',
@@ -11,7 +12,8 @@ const grid = createElement(
   Header('Yes!', 'just some text'),
   Button('Click me'),
   Button('Click this!'),
-  Card('Question?', 'Answer!')
+  Card('Question?', 'Answer!'),
+  Togglebutton()
 )
 
 document.body.append(grid)
