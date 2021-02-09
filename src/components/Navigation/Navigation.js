23782a1a558be90795d6a21299e8cff1614abec9
Robin Steinhauser <rbn2208@gmail.com>
2021-02-09 14:10:42 +0100

    stylelint fix

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,32 +7,19 @@ import createSVG from '@fortawesome/fontawesome-free/svgs/solid/plus-square.svg'
 import settingsSVG from '@fortawesome/fontawesome-free/svgs/solid/cog.svg'
 
 const buttonsConfig = [
-  // this is an array with objects for the nav elements. property and prop.value
-  { svgPath: homeSVG, text: 'Home' }, // path to the svg and the text for each button
+  { svgPath: homeSVG, text: 'Home' },
   { svgPath: bookmarkSVG, text: 'Bookmarks' },
   { svgPath: createSVG, text: 'Create' },
   { svgPath: settingsSVG, text: 'Settings' },
 ]
 
 export default function Navigation(onNavigate) {
-  // navigation steht für sich alleine als Component. Über den Parameter, hier "onNavigate"
-  // kriegt index.js zugriff auf die Funktion.
-
   const buttons = buttonsConfig.map(({ svgPath, text }) => {
-    // { src: xxx, alt: xxx } this is called destructuring assignment
-    // man könnte auch .map(button => const value = { src: xxx, alt: xxx }) schreiben
-    const button = createElement('img', { src: svgPath, alt: text }) // jeder button wird als img dargestellt mit prop.values
-    button.addEventListener('click', () => onNavigate(text)) // bei click wird über onNavigate text nach draussen gegeben
-    return button // und später die seiten getogglet
+    const button = createElement('img', { src: svgPath, alt: text })
+    button.addEventListener('click', () => onNavigate(text))
+    return button
   })
 
-  /*
-  const buttons2 = buttonsConfig.map(({ svgPath, text }) => {
-    const button2 = createElement('object', { data: svgPath, alt: text })
-    button2.addEventListener('click', () => onNavigate(text))
-    return button2
-  })
-*/
-  const el = createElement('nav', { className: 'Navigation' }, ...buttons) // creates nav and spreads all buttons in nav
+  const el = createElement('nav', { className: 'Navigation' }, ...buttons)
   return el
 }
